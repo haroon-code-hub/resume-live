bead5d8131b4405ca3ca3acd87818d9f2392240d
Haroon Saeed <haroon.saeed@outlook.com>
2026-06-17 19:21:40 +0200

    fix: replace SSH deploy with kubeconfig-based kubectl deploy
    - Remove appleboy/ssh-action, use kubectl directly from GitHub runner
    - Store kubeconfig as KUBECONFIG_DATA secret
    - Open port 6443 to allow GitHub Actions to reach k3s API

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,7 +81,7 @@ resource "aws_security_group" "k3s" {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
-    cidr_blocks = [var.allowed_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -89,7 +89,7 @@ resource "aws_security_group" "k3s" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.allowed_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
